2d095f2ff95008595189ffe9db1d779c9a161d95
blusk1n <tahaznazen@gmail.com>
2020-04-05 06:54:51 +0200

    color levels helper file added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Palette from "./components/Palette";
 import colorsPalettes from "./seedColor";
+import { generatePalette } from "./colorHelper";
 function App() {
+  console.log(generatePalette(colorsPalettes[4]));
   return (
     <div className="App">
       <Palette {...colorsPalettes[1]} />
