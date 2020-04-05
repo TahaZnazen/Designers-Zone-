8304b0910704fd5b2a835ef31833b1d3b90c5b19
blusk1n <tahaznazen@gmail.com>
2020-04-06 01:27:45 +0200

    add color  lumunosity slider

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import Palette from "./components/Palette";
 import colorsPalettes from "./seedColor";
 import { generatePalette } from "./colorHelper";
 function App() {
-  console.log(generatePalette(colorsPalettes[4]));
   return (
     <div className="App">
-      <Palette {...colorsPalettes[1]} />
+      <Palette palette={generatePalette(colorsPalettes[4])} />
     </div>
   );
 }
--- src/components/Palette.jsx
+++ src/components/Palette.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
 import ColorBox from "./colorBox/ColorBox";
 import "./palette.css";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 class Palette extends Component {
+  state = {
+    level: 500,
+  };
+  changeLevel = (level) => {
+    this.setState({
+      level,
+    });
+  };
   render() {
-    const colorBoxes = this.props.colors.map((color) => (
-      <ColorBox background={color.color} name={color.name} />
+    const { colors } = this.props.palette;
+    const { level } = this.state;
+    const colorBoxes = colors[level].map((color) => (
+      <ColorBox background={color.hex} name={color.name} />
     ));
     return (
       <div className="palette">
+        <Slider
+          step={100}
+          defaultValue={level}
+          min={100}
+          max={900}
+          onAfterChange={this.changeLevel}
+        />
         {/* navbar here */}
         <div className="palette-colors">{colorBoxes}</div>
         {/* footer */}
