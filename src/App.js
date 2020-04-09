ca13ae62f54d4a2e0a0985b9648b20efbb63ed4b
blusk1n <tahaznazen@gmail.com>
2020-04-09 09:16:29 +0200

    adding routes to the app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React from "react";
 import Palette from "./components/Palette";
 import colorsPalettes from "./seedColor";
 import { generatePalette } from "./colorHelper";
+import { Route, Switch } from "react-router-dom";
+import seedColor from "./seedColor";
 function App() {
+  const findPalette = (id) => {
+    return seedColor.find((palette) => palette.id === id);
+  };
   return (
-    <div className="App">
-      <Palette palette={generatePalette(colorsPalettes[4])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>palette list </h1>} />
+      <Route
+        exact
+        path="/palette/:id"
+        render={(routeProps) => (
+          <Palette
+            palette={generatePalette(findPalette(routeProps.match.params.id))}
+          />
+        )}
+      />
+    </Switch>
+
+    // <div className="App">
+    //   <Palette palette={generatePalette(colorsPalettes[4])} />
+    // </div>
   );
 }
 
